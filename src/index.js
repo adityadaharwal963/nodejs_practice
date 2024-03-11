e5d3fbb73506e47e8b400a4c33208853525e2285
adityadaharwal963 <adityaprowork@gmail.com>
2024-03-11 17:58:10 +0530

    express , nodemon , json

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
-const {v4 : uuidv4 } = require('uuid')
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const json = {
+    "key":"value",
+    "name":"JAG",
+    "colorArray":["red","blue"],
+    "numbers":[1,2,3,4],
+    "nest":[
+        {"id":"222",
+        "name": "hoho"}
+        ,
+        {"id":"232",
+        "name":"coco"}
+    ]
+}
+app.get('/',(req,res) =>{
+    res.send({"data":json.nest});
+})
 
-console.log(uuidv4());
-///
+app.post('/',(req,res)=> {
+    res.send('working with post req ');
+});
+app.listen(PORT,() => {
+    console.log("app listing for port 3000");
+});
+
